cf3ebbc1bc693b984460ad211bbcbfedf1995ae4
Daniel Simão <danielsimao@luego.com.br>
2019-01-29 16:03:23 -0200

    init main calc controller

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,4 +1,63 @@
-/* eslint-disable no-new */
 /**
  * MainController
  */
+
+function addEvent(nodeList, event, handler) {
+  Array.from(nodeList).forEach((node) => {
+    node.addEventListener(event, handler, false);
+  });
+}
+
+const calculatorView = function calculatorView() {
+  const elements = {
+    display: document.querySelector('#display'),
+    numbers: document.querySelectorAll('.number'),
+  };
+
+  function setDisplayValue(value) {
+    elements.display.value = value;
+  }
+
+  return {
+    elements,
+    setDisplayValue,
+  };
+};
+
+const calculatorModel = function calculatorModel() {
+  const store = {
+    total: 0,
+  };
+
+  return {
+    getTotal() {
+      return store.total;
+    },
+    setTotal(value) {
+      store.total = value;
+    },
+  };
+};
+
+const calculatorController = function calculatorController(Model, View) {
+  const model = Model();
+  const view = View();
+
+  function init() {
+    view.setDisplayValue(model.getTotal());
+
+    addEvent(view.elements.numbers, 'click', (e) => {
+      // set total
+      model.setTotal(e.target.textContent);
+      // render view
+      view.setDisplayValue(model.getTotal());
+    });
+  }
+
+  init();
+};
+
+calculatorController(
+  calculatorModel,
+  calculatorView,
+);
